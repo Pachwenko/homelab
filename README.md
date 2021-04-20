c032002a798fd9f2d7078b0e9313b6fbe1ca070c
Pachwenko <pmccart4096@gmail.com>
2021-04-20 18:52:58 -0500

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# A collections of services I may or may not use
+All docker stuff can be managed with Portainer, but additional setup will be needed on the host machine depending on what you plan to run.
+
+
+## Setup
+- Environment variables are used for sensitive info (see PLEX_CLAIM for example) so a .env file is provided
+- Some things like Plex require the /docker_mounts/plex/config folder to be created so a create_directories script is provided
+- Additionally, things like Plex also use a /mnt folder. This is not included in the create_directories script.
+
+## Running this (all of them)
+```shell
+docker-compose --env-file .env up -d
+```
+
+Navigate to portainer at http://ip_address:9000
---
+++ create_directories.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+sudo mkdir /docker_mounts
+
+sudo mkdir /docker_mounts/nextcloud
+sudo mkdir /docker_mounts/nextcloud/data
+sudo mkdir /docker_mounts/nextcloud/db
+
+sudo mkdir docker_mounts/pihole/pihole
+sudo mkdir docker_mounts/pihole/dnsmasq.d
+
+sudo mkdir docker_mounts/homeassistant
+sudo mkdir docker_mounts/homeassistant/config
+
+sudo mkdir /docker_mounts/plex
+sudo mkdir /docker_mounts/plex/config
+
+sudo mkdir /docker_mounts/unifi
+sudo mkdir /docker_mounts/unifi/config
+
+sudo mkdir /docker_mounts/influxdb
+
+sudo mkdir /docker_mounts/grafana
+
+sudo mkdir /docker_mounts/syncthing
+sudo mkdir /docker_mounts/syncthing/config
+
+sudo mkdir /docker_mounts/valheim
+sudo mkdir /docker_mounts/valheim/data
+sudo mkdir /docker_mounts/valheim/config
