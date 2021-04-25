7c0e9a9bf62ad4f2cee74e5e48bb0a12d5dba0ca
Pachwenko <pmccart4096@gmail.com>
2021-04-25 17:12:10 -0500

    refactor create_directories script

--- create_directories.sh
+++ create_directories.sh
@@ -1,47 +1,56 @@
 #! /usr/bin/env bash
 
-sudo mkdir /docker_mounts
+echo "Where do you want to put your docker mounts? (default: /docker_mounts)"
 
-sudo mkdir /docker_mounts/portainer
-sudo mkdir /docker_mounts/portainer/data
+read base_dir
 
-sudo mkdir /docker_mounts/tautulli
-sudo mkdir /docker_mounts/tautulli/config
+if [ -z "$base_dir" ]
+then
+    base_dir="/docker_mounts"
+fi
 
-sudo mkdir /docker_mounts/postgres
+sudo mkdir "$base_dir"
 
-sudo mkdir /docker_mounts/mariadb
+sudo mkdir "$base_dir"/portainer
+sudo mkdir "$base_dir"/portainer/data
 
-sudo mkdir /docker_mounts/jellyfin
-sudo mkdir /docker_mounts/jellyfin/config
+sudo mkdir "$base_dir"/tautulli
+sudo mkdir "$base_dir"/tautulli/config
 
-sudo mkdir /docker_mounts/heimdall
-sudo mkdir /docker_mounts/heimdall/config
+sudo mkdir "$base_dir"/postgres
 
-sudo mkdir /docker_mounts/nextcloud
-sudo mkdir /docker_mounts/nextcloud/data
-sudo mkdir /docker_mounts/nextcloud/db
+sudo mkdir "$base_dir"/mariadb
 
-sudo mkdir /docker_mounts/adguardhome
-sudo mkdir /docker_mounts/adguardhome/work
-sudo mkdir /docker_mounts/adguardhome/config
+sudo mkdir "$base_dir"/jellyfin
+sudo mkdir "$base_dir"/jellyfin/config
 
-sudo mkdir /docker_mounts/homeassistant
-sudo mkdir /docker_mounts/homeassistant/config
+sudo mkdir "$base_dir"/heimdall
+sudo mkdir "$base_dir"/heimdall/config
 
-sudo mkdir /docker_mounts/plex
-sudo mkdir /docker_mounts/plex/config
+sudo mkdir "$base_dir"/nextcloud
+sudo mkdir "$base_dir"/nextcloud/data
+sudo mkdir "$base_dir"/nextcloud/db
 
-sudo mkdir /docker_mounts/unifi
-sudo mkdir /docker_mounts/unifi/config
+sudo mkdir "$base_dir"/adguardhome
+sudo mkdir "$base_dir"/adguardhome/work
+sudo mkdir "$base_dir"/adguardhome/config
 
-sudo mkdir /docker_mounts/influxdb
+sudo mkdir "$base_dir"/homeassistant
+sudo mkdir "$base_dir"/homeassistant/config
 
-sudo mkdir /docker_mounts/grafana
+sudo mkdir "$base_dir"/plex
+sudo mkdir "$base_dir"/plex/config
 
-sudo mkdir /docker_mounts/syncthing
-sudo mkdir /docker_mounts/syncthing/config
+sudo mkdir "$base_dir"/unifi
+sudo mkdir "$base_dir"/unifi/config
 
-sudo mkdir /docker_mounts/valheim
-sudo mkdir /docker_mounts/valheim/data
-sudo mkdir /docker_mounts/valheim/config
+sudo mkdir "$base_dir"/influxdb
+
+sudo mkdir "$base_dir"/grafana
+
+sudo mkdir "$base_dir"/syncthing
+sudo mkdir "$base_dir"/syncthing/config
+
+sudo mkdir "$base_dir"/valheim
+sudo mkdir "$base_dir"/valheim/data
+sudo mkdir "$base_dir"/valheim/config
